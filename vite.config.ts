cd4250252637c2308f589fe832cfef33c069470e
lumetra-1 <contact@lumetra.one>
2026-06-17 12:05:38 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'app.js',
+        chunkFileNames: 'app.js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+});
